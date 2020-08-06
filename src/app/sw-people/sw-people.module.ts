f7f3fa47cfb25fcb05afe85e0acb5bbb5f524d89
snehalp29 <snehalpatkar@gmail.com>
2020-08-06 17:59:23 -0400

    updated module

--- src/app/sw-people/sw-people.module.ts
+++ src/app/sw-people/sw-people.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -7,6 +9,8 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+    HttpClientModule,
+    SharedModule,
   ]
 })
 export class SwPeopleModule { }
