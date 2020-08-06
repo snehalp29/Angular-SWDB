766ba0c6ff393a61aef7e355736773d1afecd1b9
snehalp29 <snehalpatkar@gmail.com>
2020-08-06 19:19:29 -0400

    fixed failing unit test

--- src/app/sw-people/components/people-picker/people-picker.component.spec.ts
+++ src/app/sw-people/components/people-picker/people-picker.component.spec.ts
@@ -1,16 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeoplePickerComponent } from './people-picker.component';
+import { SwService } from '../../sw.service';
 
 describe('PeoplePickerComponent', () => {
   let component: PeoplePickerComponent;
   let fixture: ComponentFixture<PeoplePickerComponent>;
+  let swService: SwService;
+
+  const swServiceSpy = jasmine.createSpyObj('SwService', [
+    'getCharacterAndFlims',
+    'getCharacters',
+  ]);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PeoplePickerComponent ]
-    })
-    .compileComponents();
+      declarations: [PeoplePickerComponent],
+      providers: [{ provide: SwService, useValue: swServiceSpy }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/sw-people/container/peoples/peoples.component.spec.ts
+++ src/app/sw-people/container/peoples/peoples.component.spec.ts
@@ -1,16 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeoplesComponent } from './peoples.component';
+import { SwService } from '../../sw.service';
 
 describe('PeoplesComponent', () => {
   let component: PeoplesComponent;
   let fixture: ComponentFixture<PeoplesComponent>;
+  let swServiceSpy = jasmine.createSpyObj('SwService', [
+    'getCharacterAndFlims',
+    'getCharacters',
+  ]);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PeoplesComponent ]
-    })
-    .compileComponents();
+      declarations: [PeoplesComponent],
+      providers: [{ provide: SwService, useValue: swServiceSpy }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
