146b0d1608d59c2441f0e265333974b3b310e91f
snehalp29 <snehalpatkar@gmail.com>
2020-08-06 18:24:32 -0400

    enabled routing

--- src/app/sw-people/sw-people.module.ts
+++ src/app/sw-people/sw-people.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from '../shared/shared.module';
-import { PeoplesComponent } from './container/peoples/peoples.component';
-import { PeopleFlimsComponent } from './components/people-flims/people-flims.component';
 import { PeoplePickerComponent } from './components/people-picker/people-picker.component';
+import { PeoplesComponent } from './container/peoples/peoples.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PeopleFlimsComponent } from './components/people-flims/people-flims.component';
+import { SharedModule } from '../shared/shared.module';
 
-
+const SWRoutes: Routes = [
+  {
+    path: '',
+    component: PeoplesComponent,
+  },
+];
 
 @NgModule({
-  declarations: [PeoplesComponent, PeopleFlimsComponent, PeoplePickerComponent],
+  declarations: [PeoplePickerComponent, PeoplesComponent, PeopleFlimsComponent],
   imports: [
-    CommonModule
+    RouterModule.forChild(SWRoutes),
+    CommonModule,
     HttpClientModule,
     SharedModule,
-  ]
+  ],
 })
-export class SwPeopleModule { }
+export class SwPeopleModule {}
