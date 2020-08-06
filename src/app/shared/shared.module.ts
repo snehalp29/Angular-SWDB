40d0a8cea67ac081969135cc7adc7506d5e9e14b
snehalp29 <snehalpatkar@gmail.com>
2020-08-06 17:46:34 -0400

    Added loading pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { WithLoadingPipe } from './with-loading.pipe';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [WithLoadingPipe],
+  imports: [CommonModule],
+  exports: [WithLoadingPipe],
 })
-export class SharedModule { }
+export class SharedModule {}
